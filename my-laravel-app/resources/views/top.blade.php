82fb06b3bcbacfba5102c88aeb9644d5f69cc150
yasu-44hiro <44hiro.28@gmail.com>
2020-12-30 12:42:00 +0900

    トップ　背景まで

--- my-laravel-app/resources/views/top.blade.php
+++ my-laravel-app/resources/views/top.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-sm">
-  <h1 class="text-center">My Supplement</h1>
-  <p class="text-center">お気に入りのSupplementを見つけよう</p>
-  <p class="text-center"><a href="{{ route('login') }}" type="button" class="btn btn-outline-primary btn-lg " >始める</a></p>
-</div>
+<div class="container-fluid d-flex vh-100 hero">
+    <div class="container row justify-content-center align-self-center">
+      <h1 class="text-center mt-n5">My Supplement</h1>
+      <p class="text-center mt-5">お気に入りのSupplementを見つけよう</p>
+      <p class="text-center mt-5"><a href="{{ route('login') }}" type="button" class="btn btn-outline-primary btn-lg " >始める</a></p>
+    </div>
+  </div>
 @endsection
