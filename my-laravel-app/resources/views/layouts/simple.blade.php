3da5b96722da6d2e7ecdd70be94b5636c0c4fb25
yasu-44hiro <44hiro.28@gmail.com>
2021-01-29 06:51:18 +0900

    メタタグ追加

--- my-laravel-app/resources/views/layouts/simple.blade.php
+++ my-laravel-app/resources/views/layouts/simple.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>My Supplement</title>
+    <meta name="description" content="お気に入りのサプリメントを投稿できるアプリです。お気に入りのプロテインやサプリメントをシェアしよう。" />
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
