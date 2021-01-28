451160c04202c6e02de726fd1a1f57dd1c4f740f
yasu-44hiro <44hiro.28@gmail.com>
2021-01-29 05:49:47 +0900

    ゲストログインの修正

--- my-laravel-app/database/seeds/UserSeeder.php
+++ my-laravel-app/database/seeds/UserSeeder.php
@@ -12,21 +12,21 @@ class UserSeeder extends Seeder
     public function run()
     {
         //管理者（ログイン用)
-        factory(App\User::class)
-            ->create([
-                'name' => '吉廣泰則',
-                'email' => 'yoshihiro@example.com',
-                'password' => bcrypt('password'),
-                'role' => 'owner'
-            ]);
+        //factory(App\User::class)
+            //->create([
+                //'name' => '吉廣泰則',
+                //'email' => 'yoshihiro@example.com',
+                //'password' => bcrypt('password'),
+                //'role' => 'owner'
+            //]);
         // 一般ユーザー
-        factory(App\User::class)
-            ->create([
-                'name' => '立花春雄',
-                'email' => 'haruo@example.com',
-                'password' => bcrypt('password'),
-                'role' => 'customer'
-            ]);
-        factory(App\User::class, 10)->create();
+        //factory(App\User::class)
+            //->create([
+                //'name' => '立花春雄',
+                //'email' => 'haruo@example.com',
+                //'password' => bcrypt('password'),
+                //'role' => 'customer'
+            //]);
+        //factory(App\User::class, 10)->create();
     }
 }
